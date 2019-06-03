f1bcbaff7f01159c29130f7c98753fa75bf5b43c
TheMuellTrain <44850044+TheMuellTrain@users.noreply.github.com>
2019-06-03 12:17:24 -0500

    Updated PurgeFiles.py
    Updated project structure and revised comments

--- PurgeFiles/PurgeFiles/PurgeFiles.py
+++ PurgeFiles/PurgeFiles/PurgeFiles.py
@@ -9,52 +9,41 @@ import logging.config
 
 
 
-source = r'C:\Users\Desktop\BetaSource'
-dest = r'C:\Users\Desktop\BetaDest'
-#Gets the current time from the time module
-now = time.time()
-#Timer of when to purge files
-cutoff = now - (14 * 86400)
+source = r'C:\Users\q155344\Desktop\BetaSource'
+dest = r'C:\Users\q155344\Desktop\BetaDest'
+#A timer was needed to know when to purge files 
+cutoff = time.time() - (14 * 86400)
 source_list = []
 all_sources = []
-all_dest_dirty = []
 logging.basicConfig(level = logging.INFO, filename = time.strftime("main-%Y-%m-%d.log"))
-
-def main():
-	dest_files()
-	purge_files()
-#I used the dess_files function to get all of the destination files
+#I had to create a funciton that would grab all the destination files.
 def dest_files():
+	dirty_files = []
 	for dest_root, dest_subdirs, dest_files in os.walk(dest):
 		for f in dest_files:
-			global All_dest_dirty
-			all_dest_dirty.append(f)	
-			
+			dirty_files.append(f)
+	return dirty_files		
+all_dest_dirty = dest_files()
 def purge_files():
 	logging.info('invoke purge_files method')
 	#I removed all duplicates from dest because cleaning up duplicates in dest is out of the scope
-	all_dest_clean = list(dict.fromkeys(all_dest_dirty))
-	#os.walk used to get all files in the source location 
+	all_dest_clean = set(all_dest_dirty)
+	#os.walk was used because I needed to get a lost of all the files in the source location
 	for source_root, source_subdirs, source_files in os.walk(source):
-		#looped through every file in source_files
 		for f in source_files:
-			#appending all_sources to get the application name from the file path
+			#I needed just the application name from the full path to compare its value to the destination. The destination set is just the application name not the full path.
 			all_sources.append(os.path.abspath(f).split('\\')[-1]) 
-			#looping through each element of all_source
 			for i in all_sources:
-				#logical check to see if file in the source folder exists in the destination folder
+				# I needed to check if the source file was in the destination. 
 				if i not in all_dest_clean:
-					#src is used to get the path of the source file this will be needed to move the file in shutil.move
 					src =  os.path.abspath(os.path.join(source_root, i))
-					#the two variables used below are to get the creation time of the files
 					t = os.stat(src)
 					c = t.st_ctime
-					#logical check to see if the file is older than the cutoff
+					#I only want to move the files that are older then the defined cutoff
 					if c<cutoff:
 						logging.info(f'File has been succesfully moved: {i}')
 						print(f'File has been succesfully moved: {i}')
 						shutil.move(src,dest)
-						#removing the allready checked source files for the list this is also used in other spots within the loop
 						all_sources.remove(i)
 					else:
 						logging.info(f'File is not older than 14 days: {i}')
@@ -64,5 +53,10 @@ def purge_files():
 					all_sources.remove(i)
 					logging.info(f'File: {i} allready exists in the destination')
 					print(f'File: {i} allready exists in the destination')
+
+def main():
+	dest_files()
+	purge_files()
+
 if __name__ == '__main__':
     main()
